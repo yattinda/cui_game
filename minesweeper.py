d844ed52246e283583fecf13eaacbc9cb71ade25
yattinda <yattinda@gmail.com>
2021-06-25 23:10:57 +0900

    Fix error

--- minesweeper.py
+++ minesweeper.py
@@ -1,25 +1,29 @@
 import random
 
+def checkRange(grid, p, q):
+    try:
+        grid[q][p + 1] += 100
+    except Exception:
+        pass
+
 def setBoard(gridNum, mineNum):
     grid = [[-1] * gridNum for i in range(gridNum)]
     count = 0
     while(count <= mineNum):
         p = random.randint(0, gridNum - 1)
         q = random.randint(0, gridNum - 1)
-        # print(p, q)
-        # print(grid[q][p])
         if(grid[q][p] > -2):
 
             grid[q][p] += -10000
 
-            grid[q][p + 1] += 100
-            grid[q][p - 1] += 100
-            grid[q + 1][p - 1] += 100
-            grid[q + 1][p] += 100
-            grid[q + 1][p + 1] += 100
-            grid[q - 1][p - 1] += 100
-            grid[q - 1][p] += 100
-            grid[q - 1][p + 1] += 100
+            checkRange(grid, p-1, q-1)
+            checkRange(grid, p-1, q)
+            checkRange(grid, p-1, q+1)
+            checkRange(grid, p, q-1)
+            checkRange(grid, p, q+1)
+            checkRange(grid, p+1, q)
+            checkRange(grid, p+1, q+1)
+            checkRange(grid, p+1, q)
 
             count += 1
 
